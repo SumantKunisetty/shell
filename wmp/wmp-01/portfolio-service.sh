36520f2c20339d3d27fe6d32c04f92543fc02bf9
Sumant-gituser <sumant@git.com>
2026-09-20 12:38:08 +0530

    added source.sh

--- wmp/wmp-01/portfolio-service.sh
+++ wmp/wmp-01/portfolio-service.sh
@@ -1,13 +1,15 @@
 source common.sh
-service_name=auth-service
-dnf install -y golang
+service_name=portfolio-service
+dnf install -y java-21-openjdk-devel
+java -version
 cp ${service_name}.service /etc/systemd/system/
 app_prereq
 cd /app
-CGO_ENABLED=0 go build -o ${service_name} ./cmd/server
+chmod +x gradlew
+./gradlew bootJar --no-daemon -x test
+cp /app/build/libs/*.jar /app/${service_name}.jar
 chown -R appuser:appuser /app
 chmod o-rwx /app -R
-#systemctl commands
 systemctl daemon-reload
 systemctl enable ${service_name}
 systemctl start ${service_name}
