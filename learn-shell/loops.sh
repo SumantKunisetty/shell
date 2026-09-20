5539c512fc7d9f29e34ec738bafbb1f680b64b45
Sumant-gituser <sumant@git.com>
2026-09-20 13:39:26 +0530

    added port check

--- learn-shell/loops.sh
+++ learn-shell/loops.sh
@@ -7,3 +7,10 @@ while [ $x -gt 0 ]; do
     echo "value: $x"
     x=$((x-1))
 done
+
+nc -w 2 -z localhost 3000
+while [ $? -ne 0 ]; do
+    sleep 2
+    echo trying again after 2 seconds
+    nc -w 2 -z localhost 3000
+done
