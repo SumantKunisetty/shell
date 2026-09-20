5e8419cf6aac044c5327482ba34380d5759569a9
Sumant-gituser <sumant@git.com>
2026-09-20 12:45:25 +0530

    added if

--- wmp/wmp-01/common.sh
+++ wmp/wmp-01/common.sh
@@ -1,5 +1,10 @@
 app_prereq(){
-    useradd -r -s /bin/false appuser
+    id appuser &>/dev/null
+    if [ $? -eq 0]; then
+        echo "app user already exists"
+    else:
+        useradd -r -s /bin/false appuser
+    fi
     mkdir -p /app
     curl -L -o /tmp/${service_name}.tar.gz https://raw.githubusercontent.com/raghudevopsb88/wealth-project/main/artifacts/${service_name}.tar.gz
     cd /app
