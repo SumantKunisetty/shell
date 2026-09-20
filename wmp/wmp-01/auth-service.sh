dc18d37bf71b30b6566602065740a7da62438430
Sumant-gituser <sumant@git.com>
2026-09-20 12:16:51 +0530

    added $

--- wmp/wmp-01/auth-service.sh
+++ wmp/wmp-01/auth-service.sh
@@ -1,13 +1,13 @@
 service_name=auth-service
 dnf install -y golang
-cp {service_name}.service /etc/systemd/system/
+cp ${service_name}.service /etc/systemd/system/
 useradd -r -s /bin/false appuser
 mkdir -p /app
-curl -L -o /tmp/{service_name}.tar.gz https://raw.githubusercontent.com/raghudevopsb88/wealth-project/main/artifacts/{service_name}.tar.gz
+curl -L -o /tmp/${service_name}.tar.gz https://raw.githubusercontent.com/raghudevopsb88/wealth-project/main/artifacts/${service_name}.tar.gz
 cd /app
-tar xzf /tmp/{service_name}.tar.gz
+tar xzf /tmp/${service_name}.tar.gz
 cd /app
-CGO_ENABLED=0 go build -o {service_name} ./cmd/server
+CGO_ENABLED=0 go build -o ${service_name} ./cmd/server
 chown -R appuser:appuser /app
 chmod o-rwx /app -R
 #systemctl commands
